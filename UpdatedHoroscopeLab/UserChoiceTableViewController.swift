77c0c3919379b54d9d507bd46ec93af1fda9b01d
Yuliia Engman <yuliia.engman@gmail.com>
2020-01-22 15:03:17 -0500

    set layout for VCs

---
+++ UpdatedHoroscopeLab/UserChoiceTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UserChoiceTableViewController.swift
+//  UpdatedHoroscopeLab
+//
+//  Created by Yuliia Engman on 1/22/20.
+//  Copyright © 2020 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class UserChoiceTableViewController: UITableViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 3
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 1
+//    }
+
+//    tableview
+ 
+    @IBAction func horoscopeButtonPressed(_ sender: UIButton) {
+    }
+    
+}
